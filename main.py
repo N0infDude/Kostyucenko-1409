bd966ee82a3ce89fce67899b4bd24cc1fb99250c
Andrey Kostyuchenko <128247995+N0infDude@users.noreply.github.com>
2024-03-15 18:37:33 +0300

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ with open('game.txt','r',encoding = 'utf-8') as f:
 str_l = ''.join(l)
 str_l = str_l.replace('$','/')
 print(str_l)
+ans = open('game_new.txt','w',encoding = 'utf-8')
 f.close()
-
-    
+ans.close()
